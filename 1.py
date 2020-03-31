22fc4424da465d3573ad3fe19f3721decb7bd9c0
Zixu Jiang <zixu.jiang1996@gmail.com>
2020-03-31 10:59:38 -0400

    3.30

--- 1.py
+++ 1.py
@@ -1,7 +1,5 @@
-import re 
-address = '123add 333 21'
-test1 = re.sub('[\d]+','',address)
-test2 = re.sub('[\d]+','',address, 1)
-print(test1)
-print(test2)
-
+li = ["a", "b", "d", "d"]
+resultList = []
+for elem in li:
+    list(set(li))
+print(li)
